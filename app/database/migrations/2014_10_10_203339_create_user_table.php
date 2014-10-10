c4a8ebd85d7898aeb552d7ddb7352d45be4cf49b
Jedidiah <ferrer.jedidiah@gmail.com>
2014-10-10 14:07:28 -0700

    add migrations

---
+++ app/database/migrations/2014_10_10_203339_create_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+		{
+		    $table->increments('id');
+		    $table->string('name');
+		    $table->string('nickname');
+		    $table->string('email');
+		    $table->string('password');
+		    $table->rememberToken();
+		    $table->string('img_path');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
